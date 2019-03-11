41889277b43394d4421b59892da2cfdaaab99416
何文林 <370215230@qq.com>
2019-03-11 17:57:21 +0800

    init

---
+++ main.js
@@ -0,0 +1,38 @@
+const electron = require('electron')
+const { app, BrowserWindow } = electron
+let mainWindow
+
+function createWindow () {
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
+  mainWindow = new BrowserWindow({
+    width,
+    height,
+    webPreferences: {
+      nodeIntegration: true,
+      nativeWindowOpen: false
+    }
+  })
+  mainWindow.loadFile('index.html')
+  // mainWindow.loadURL('https://www.kye-erp.com')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
